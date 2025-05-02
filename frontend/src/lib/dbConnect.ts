1ba403f21ffd6e1080881b2a37c0fb432eac0ec2
zubair mallik <zubair7035@gmail.com>
2025-05-02 12:18:39 +0530

    testing the connection to db

--- frontend/src/lib/dbConnect.ts
+++ frontend/src/lib/dbConnect.ts
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
+ try{
   if (mongoose.connections[0].readyState) {
     return;
   }
   await mongoose.connect(process.env.MONGO_URI!);
+ }
+ catch(e){
+  console.log(e)
+ }
 }
